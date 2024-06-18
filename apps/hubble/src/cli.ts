1a5229a5ee5d12ca5a0a7cf6dcae9170ec1d4695
csltx <153992247+csltx@users.noreply.github.com>
2024-06-18 20:56:18 +0100

    Update cli.ts
    fix amount of memory

--- apps/hubble/src/cli.ts
+++ apps/hubble/src/cli.ts
@@ -242,10 +242,10 @@ app
     // detects it as slightly under that depending on OS, so we'll only error if
     // it's less than 15GB.
     const totalMemory = Math.floor(os.totalmem() / 1024 / 1024 / 1024);
-    if (totalMemory < 15) {
+    if (totalMemory < 3) {
       startupCheck.printStartupCheckStatus(
         StartupCheckStatus.ERROR,
-        `Hubble requires at least 16GB of RAM to run. Detected ${totalMemory}GB`,
+        `Hubble requires at least 3GB of RAM to run. Detected ${totalMemory}GB`,
       );
       return flushAndExit(1);
     } else {
